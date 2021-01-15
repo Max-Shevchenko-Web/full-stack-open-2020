861f28f65aaaa1bafb5adc779464848c4ebadf1c
Шевченко Максим <shevchenko@gogpb.ru>
2021-01-15 17:05:59 +0200

    init unicafe app

--- part_1/course_information/src/components/Total.js
+++ part_1/course_information/src/components/Total.js
@@ -7,7 +7,7 @@ const Total = ({ parts }) => {
       totalExercises += part.exercises;
     });
   }
-  return <p>Number of exercises {totalExercises}</p>;
+  return <p>Number of Total exercises {totalExercises}</p>;
 };
 
 export default Total;
