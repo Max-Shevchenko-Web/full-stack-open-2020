6427c226f7de1fa769fd7bde9a0359140e9f8580
Шевченко Максим <shevchenko@gogpb.ru>
2021-01-18 17:32:14 +0200

    finished 2.1-2.5

--- part_2/course_information/src/components/Total.js
+++ part_2/course_information/src/components/Total.js
@@ -1,13 +1,10 @@
 import React from "react";
 
 const Total = ({ parts }) => {
-  let totalExercises = 0;
-  if (parts) {
-    parts.forEach(part => {
-      totalExercises += part.exercises;
-    });
-  }
-  return <p>Number of Total exercises {totalExercises}</p>;
+  let totalExercises = parts.reduce((acc, cur)=> {
+    return acc + cur.exercises
+  }, 0)
+  return <p><b>Total of {totalExercises} exercises</b></p>;
 };
 
 export default Total;
