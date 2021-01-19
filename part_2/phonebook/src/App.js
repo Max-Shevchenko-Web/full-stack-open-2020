8bfd2b1c76b6680a0b3230143c04cd8d008f68ad
Шевченко Максим <shevchenko@gogpb.ru>
2021-01-19 17:13:37 +0200

    2.15-2.18

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import personsService from './services/personsService'
 
-import axios from 'axios'
 
 const App = () => {
   const [ persons, setPersons ] = useState([])
@@ -13,36 +13,96 @@ const App = () => {
 
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personsService
+      .getAll()
+      .then(persons => {
+        setPersons(persons);
       })
   }, [])
 
-  const addNote =(event) => {
+  const addPerson =(event) => {
     event.preventDefault();
-
-    const isContains = persons.some(item => item.name === newName);;
-    console.log(isContains);
-    if (isContains) {
-      alert(`${newName} is already added to phonebook`)
-      return
-    }
 
     const newObject = {
       name: newName,
       number: newNumber
     }
 
-    setPersons(persons.concat(newObject));
-    setNewName('');
-    setNewNumber('');
-  }
+    // if such a name is already in the list
+    const isContains = persons.some(item => {
+      if (item.name === newName) {
+        // suggest updating
+        const result = window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`);
+
+        if (result) {
+          personsService
+              .update(item.id, newObject)
+              .then(data => {
+                // update the list only in the place we need
+                setPersons(persons.map(person => person.id !== item.id ? person : data))
+                // dispatchNotification({
+                //     message : `${newName} updated`,
+                //     typeOfClass : 'info'
+                // })
+              })
+        }
+        return true
+      } else {
+        return false
+      }
+    });
+
+    // if this name does not exist, create a new one
+    if(!isContains) {
+      personsService.create(newObject).then(person=> {
+        setPersons(persons.concat(person));
+        setNewName('');
+        setNewNumber('');
+        // dispatchNotification({
+        //   message : `Added ${person.name}`,
+        //   typeOfClass : 'info'
+        // })
+      })
+      .catch(error => {
+        console.log(error.response.data)
+        // dispatchNotification({
+        //   // message: JSON.stringify(error.response.data),
+        //   message: `${newName} not added`,
+        //   typeOfClass: 'error'
+        // })
+      })
+    }
+}
 
   const toCompare = (str1, str2) => {
     const regExp = new RegExp(str2, 'gi')
     return regExp.test(str1.toString())
+  }
+
+  const deleteNote = ({name, id}) => {
+    const result = window.confirm(`Delete ${name} ?`);
+    if (result) {
+      personsService
+        .deletePerson(id)
+        .then(() => {
+          setPersons(persons.filter(p => p.id !== id))
+          // dispatchNotification({
+          //   message : `${name} was removed`,
+          //   typeOfClass : 'info'
+          // })
+        })
+        .catch(error => {
+          console.log(error);
+          // dispatchNotification({
+          //   message: `Information of ${name} has already been removed from server`,
+          //   typeOfClass: 'error'
+          // })
+          setPersons(persons.filter(p => p.id !== id))
+        })
+
+    } else {
+      return null
+    }
   }
 
   const numbersToShow = search
@@ -55,14 +115,14 @@ const App = () => {
       <Filter  value={search} onChange={(event) => setSearch(event.target.value)}/>
       <h2>Add a new numbers</h2>
       <PersonForm
-        onSubmit={addNote}
+        onSubmit={addPerson}
         newName={newName}
         newNumber={newNumber}
         onChangeName={(event) => setNewName(event.target.value)}
         onChangeNumber={(event) => setNewNumber(event.target.value)}
       />
       <h2>Numbers</h2>
-      <Persons numbersToShow={numbersToShow} />
+      <Persons numbersToShow={numbersToShow} deleteNote={deleteNote} />
     </div>
   )
 }
--- part_2/phonebook/src/components/Persons.js
+++ part_2/phonebook/src/components/Persons.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Persons = ({numbersToShow}) => {
+const Persons = ({numbersToShow, deleteNote}) => {
   return (
     <ul>
       {numbersToShow.map(person => {
-        return <li key={person.name} >{person.name}:{person.number}</li>
+        return (
+          <div key={person.name} style={{display: 'flex'}}>
+            <li  style={{marginRight: '5px'}}>{person.name}:{person.number}</li>
+            <button onClick={() => deleteNote(person)}>delete</button>
+          </div>
+        )
       })}
     </ul>
   )
